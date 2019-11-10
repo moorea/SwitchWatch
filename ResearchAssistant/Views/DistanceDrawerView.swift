d5aa863a431edfc6b24e607f9b8a2204128951cf
Andrew Moore <admoore14@gmail.com>
2019-11-10 09:36:44 -0600

    calibration and conversion to real world distances working basically

--- ResearchAssistant/Views/DistanceDrawerView.swift
+++ ResearchAssistant/Views/DistanceDrawerView.swift
@@ -8,61 +8,142 @@
 
 import SwiftUI
 
-extension CGPoint {
-    func distance(to point: CGPoint) -> CGFloat {
-        return sqrt(pow(x - point.x, 2) + pow(y - point.y, 2))
-    }
+enum DistanceDrawingState {
+    case selectingVideo
+    case calibrating
+    case observing
 }
 
-struct Drawing {
-    var points: [CGPoint] = [CGPoint]()
-    var totalDistance: CGFloat {
-        var totalDistance = CGFloat(0)
-        guard points.count > 1 else {
-            return 0
-        }
-        for (index, point) in points.enumerated() {
-            guard index < points.count - 1 else {
-                break
-            }
-            totalDistance += point.distance(to: points[index + 1])
-        }
-        return totalDistance
-    }
+struct DistanceDrawer {
+    var state: DistanceDrawingState = .selectingVideo
+    
+    var calibrationDrawing: Drawing = Drawing()
+    var drawingOne: Drawing = Drawing()
+    var drawingTwo: Drawing = Drawing()
+    
 }
 
 struct DistanceDrawerView: View {
+    @State private var state: DistanceDrawingState = .selectingVideo
+    
+    @State private var calibrationDrawing: Drawing = Drawing()
+    @State private var calibrationDistance: String = "5"
+    
     @State private var drawingOne: Drawing = Drawing()
     @State private var drawingTwo: Drawing = Drawing()
 
-    @State var isAutoReplay: Bool = false
-    @State var isPlay: Bool = true
-    @State var isMute: Bool = true
+    @State private var isAutoReplay: Bool = false
+    @State private var isPlay: Bool = true
+    @State private var isMute: Bool = true
     
-    @State var processor: VideoFrameOverlayProcessor?
+    @State private var processor: VideoFrameOverlayProcessor?
     
+    @State private var distanceRatio: CGFloat = 0.0
     
     var body: some View {
-        VStack {
-            VideoPickerButton { videoURL in
-                self.processor = VideoFrameOverlayProcessor(videoFileURL: videoURL)
-            }
-            Text("Distance 1: \(String(format: "%.1f", drawingOne.totalDistance))")
-                .font(.largeTitle)
-            Text("Distance 2: \(String(format: "%.1f", drawingTwo.totalDistance))")
-                .font(.largeTitle)
-            
-            ZStack (alignment: .top) {
-                if processor?.url != nil {
-                    VideoPlayerView(url: .constant(processor!.url), isPlay: $isPlay)
-                        .autoReplay($isAutoReplay)
-                        .mute($isMute)
-                        .onPlayToEndTime { print("Play to the end time.") }
-                        .onReplay { print("Replay after playing to the end.") }
-                        .onStateChanged { _ in print("Playback status changes, such as from play to pause.") }
+        VStack(alignment: .leading) {
+            if state == .selectingVideo {
+                HStack {
+                    Text("Step 1: Select Video")
+                        .font(.largeTitle)
+                    Spacer()
                 }
                 
-                DrawingPad(drawingOne: $drawingOne, drawingTwo: $drawingTwo)
+                VideoPickerButton { videoURL in
+                    self.processor = VideoFrameOverlayProcessor(videoFileURL: videoURL)
+                    self.state = .calibrating
+                }
+                Spacer()
+            }
+            else if state == .calibrating {
+                VStack(alignment: .leading, spacing: 4.0) {
+                    Text("Step 2: Calibrate")
+                        .font(.largeTitle)
+                    HStack {
+                        Text("Drawing a ")
+                        TextField("#", text: $calibrationDistance)
+                            .frame(width: 50, alignment: .center)
+                            .keyboardType(.numberPad)
+                        Text("cm line")
+                        Spacer()
+                        Button(action: {
+                            guard let realWorldDistance = Float(self.calibrationDistance) else {
+                                return
+                            }
+                            self.distanceRatio = CGFloat(realWorldDistance) / self.calibrationDrawing.totalDistance
+                            self.state = .observing
+                        }) {
+                            Text("Done")
+                        }
+                    }
+                    ZStack (alignment: .top) {
+                        if processor?.url != nil {
+                            VideoPlayerView(url: .constant(processor!.url), isPlay: .constant(true))
+                                .autoReplay(.constant(false))
+                                .mute(.constant(true))
+                        }
+                        
+                        CalibrationPad(drawing: $calibrationDrawing)
+                    }
+                }
+            }
+            else if state == .observing {
+                Text("Step 3: Observe")
+                    .font(.largeTitle)
+                Text("Distance 1: \(String(format: "%.1f cm", drawingOne.realWorldDistance(with: distanceRatio)))")
+                    .font(.subheadline)
+                Text("Distance 2: \(String(format: "%.1f cm", drawingTwo.realWorldDistance(with: distanceRatio)))")
+                    .font(.subheadline)
+                
+                ZStack (alignment: .top) {
+                    if processor?.url != nil {
+                        VideoPlayerView(url: .constant(processor!.url), isPlay: $isPlay)
+                            .autoReplay($isAutoReplay)
+                            .mute($isMute)
+                            .onPlayToEndTime { print("Play to the end time.") }
+                            .onReplay { print("Replay after playing to the end.") }
+                            .onStateChanged { _ in print("Playback status changes, such as from play to pause.") }
+                    }
+                    
+                    DrawingPad(drawingOne: $drawingOne, drawingTwo: $drawingTwo)
+                }
+            }
+        }
+        .padding()
+    }
+}
+
+struct CalibrationPad: View {
+    @Binding var drawing: Drawing
+
+    var body: some View {
+        GeometryReader { geometry in
+            Path { path in
+                self.add(drawing: self.drawing, toPath: &path)
+            }
+            .stroke(Color.black, lineWidth: 2.0)
+            .background(Color(hue: 0.0, saturation: 0.0, brightness: 0.0, opacity: 0.1))
+            .gesture(
+                DragGesture(minimumDistance: 0.1)
+                    .onChanged({ (value) in
+                        let currentPoint = value.location
+                        if currentPoint.y >= 0 && currentPoint.y < geometry.size.height {
+                            self.drawing.points.append(currentPoint)
+                        }
+                    })
+            )
+        }
+        .frame(maxHeight: .infinity)
+    }
+    
+    private func add(drawing: Drawing, toPath path: inout Path) {
+        let points = drawing.points
+        if points.count > 1 {
+            for i in 0..<points.count-1 {
+                let current = points[i]
+                let next = points[i+1]
+                path.move(to: current)
+                path.addLine(to: next)
             }
         }
     }
