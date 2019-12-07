b7a83d94e9e8bf799daa382040bd356a64ceae53
Andrew Moore <admoore14@gmail.com>
2019-12-07 10:03:58 -0600

    StackNavigationViewStyle

--- ResearchAssistant/Views/HomeView.swift
+++ ResearchAssistant/Views/HomeView.swift
@@ -42,6 +42,7 @@ struct HomeView: View {
             }
             .navigationBarTitle("Tools", displayMode: .large)
         }
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
